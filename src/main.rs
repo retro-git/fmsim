3d37da193606dbb00c996da0764dccbb247020ce
retro <test@test.com>
2023-09-14 10:42:24 +0100

    snake_case

--- src/main.rs
+++ src/main.rs
@@ -5,69 +5,74 @@ use diesel::sqlite::SqliteConnection;
 
 // Include the schema
 mod schema;
-use schema::CardTypes;
-use schema::Cards; // Add this line
-use schema::Fusions; // Add this line
+use schema::card_types;
+use schema::cards; // Add this line
+use schema::fusions; // Add this line
 
 #[derive(Queryable, Debug)]
 struct CardType {
-    TypeID: i32,
-    Type: String,
-    IsMonster: i32,
+    type_id: i32,
+    type_: String,
+    is_monster: i32,
 }
 
 #[derive(Queryable, Debug)] // Add this struct
 struct Card {
-    CardID: i32,
-    Name: String,
-    Description: String,
-    GuardianStarAID: i32,
-    GuardianStarBID: i32,
-    Level: i32,
-    TypeID: i32,
-    Attack: i32,
-    Defense: i32,
-    Stars: i32,
-    CardCode: i32,
-    Attribute: i32,
-    NameColor: i32,
-    DescColor: i32,
-    AbcSort: i32,
-    MaxSort: i32,
-    AtkSort: i32,
-    DefSort: i32,
-    TypSort: i32,
-    AISort: i32,
-    AiGs: Option<i32>,
+    card_id: i32,
+    name: String,
+    description: String,
+    guardian_star_a_id: i32,
+    guardian_star_b_id: i32,
+    level: i32,
+    type_id: i32,
+    attack: i32,
+    defense: i32,
+    stars: i32,
+    card_code: i32,
+    attribute: i32,
+    name_color: i32,
+    desc_color: i32,
+    abc_sort: i32,
+    max_sort: i32,
+    atk_sort: i32,
+    def_sort: i32,
+    typ_sort: i32,
+    ai_sort: i32,
+    ai_gs: Option<i32>,
 }
 
 #[derive(Queryable, Debug, Clone)]
 struct Fusion {
-    FusionID: i32,
-    Card1ID: i32,
-    Card2ID: i32,
-    ResultCardID: i32,
+    fusion_id: i32,
+    card1_id: i32,
+    card2_id: i32,
+    result_card_id: i32,
 }
 
 fn get_fusions_for_card(card_id: i32, fusions: &Vec<Fusion>) -> Vec<Fusion> {
     fusions
         .iter()
-        .filter(|fusion| fusion.Card1ID == card_id || fusion.Card2ID == card_id)
+        .filter(|fusion| fusion.card1_id == card_id || fusion.card2_id == card_id)
         .cloned()
         .collect()
 }
 
+fn get_fusions_for_card2(card_id: i32, connection: &mut SqliteConnection) -> QueryResult<Vec<Fusion>> {
+    use schema::fusions::dsl::*;
+    fusions.filter(card1_id.eq(card_id).or(card2_id.eq(card_id))).load(connection)
+}
+
 fn main() {
 
-    let database_url = "data/FMDatabase.db";
+    let database_url = "data/fm.db";
     let mut connection = SqliteConnection::establish(&database_url)
         .unwrap_or_else(|_| panic!("Error connecting to {}", database_url));
 
-    use schema::CardTypes::dsl::*;
-    use schema::Cards::dsl::*;
-    use schema::Fusions::dsl::*; // Add this line
+    use schema::card_types::dsl::*;
+    use schema::cards::dsl::*;
+    use schema::fusions::dsl::*; // Add this line
 
-    let results = CardTypes.load::<CardType>(&mut connection)
+    let results = card_types.load::<CardType>(&mut connection)
         .expect("Error loading CardTypes");
 
     println!("Displaying {} card types", results.len());
@@ -75,7 +80,7 @@ fn main() {
         println!("{:?}", card_type);
     }
 
-    let results = Cards.load::<Card>(&mut connection)
+    let results = cards.load::<Card>(&mut connection)
         .expect("Error loading Cards");
 
     println!("Displaying {} cards", results.len());
@@ -83,12 +88,12 @@ fn main() {
         println!("{:?}", card);
     }
 
-    let fusions = Fusions.load::<Fusion>(&mut connection)
+    let fusions_vec = fusions.load::<Fusion>(&mut connection)
         .expect("Error loading Fusions");
 
-    let card_id = 425;
-    let fusions_for_card = get_fusions_for_card(card_id, &fusions);
-    println!("Displaying fusions for card with id {}", card_id);
+    let td_card_id = 425;
+    let fusions_for_card = get_fusions_for_card(td_card_id, &fusions_vec);
+    println!("Displaying fusions for card with id {}", td_card_id);
     for fusion in fusions_for_card {
         println!("{:?}", fusion);
     }
